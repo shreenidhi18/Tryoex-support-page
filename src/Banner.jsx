eee2d1be05e8f775b8454197be553f78ad15dbe8
Shreenidhi-V <151930484+Shreenidhi-V@users.noreply.github.com>
2024-03-12 23:15:47 +0530

    images chnaged to public diurectory

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -4,9 +4,9 @@ export default function Banner(){
         <div className="banner-text">
         <h1>Tryoex-Track your expenses</h1>
         <p>Take control of your finances with Tryoex, the easy-to-use expense tracker that helps you manage your money on the go. Whether you're tracking personal expenses or business costs, Tryoex makes it simple to stay on top of your budget and financial goals.</p>
-        <a href=""><img style={{width: '250px',height: '100px',paddingTop:'50px'}} src="src/assets/appstore.svg" alt="" /></a>
+        <a href=""><img style={{width: '250px',height: '100px',paddingTop:'50px'}} src="/images/appstore.svg" alt="" /></a>
         </div>
-        <img src="/src/assets/branding.png" alt="" />
+        <img src="/images/branding.png" alt="" />
         <br />
     </div>
     </>);
